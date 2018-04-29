248d65f74c296fe7afbdd9021b6f088fcfbe4369
sorker3 <sorker4@hotmail.com>
2018-04-29 13:26:18 +0200

    Börjat testa ett annat sätt att göra det på. Ej klart

--- out/game.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var scoreManager_1 = require("./scoreManager");
-var mgr = scoreManager_1["default"].getInstance();
-mgr.increase;
-console.log(mgr.count);
--- scoreManager.ts
+++ scoreManager.ts
@@ -2,22 +2,16 @@ import score from './score';
 
 export default class scoreManager {
     private static _instance: scoreManager;
-    private _score: any;
-    public count: number = 0;
+    private _score: score = score.getInstance();
 
     private constructor() {}
 
-    public init(count: number) {
-        this._score = score.getInstance();
-        this.count = this._score.count;
-    }
-
     public increase() {
-        this.count++
+        this._score.count++
     }
 
     public static getInstance() {
-        if (scoreManager._instance) {
+        if (!scoreManager._instance) {
             scoreManager._instance = new scoreManager();
         }
         return scoreManager._instance;
